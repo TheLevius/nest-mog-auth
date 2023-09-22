4b81f2a1491092b3758c49f4073903be5ab1a29c
reflevius <reflevius@gmail.com>
2023-09-22 23:23:10 +0300

    feat: add crud user api

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Delete,
+	Get,
+	Param,
+	ParseUUIDPipe,
+	Post,
+} from '@nestjs/common';
+import { UserService } from './user.service';
+import { CreateUserDto } from './dto/create.user.dto';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+	constructor(private readonly userService: UserService) {}
+
+	@Post()
+	createUser(@Body() dto: CreateUserDto) {
+		return this.userService.save(dto);
+	}
+
+	@Get(':idOrEmail')
+	findOneUser(@Param('idOrEmail') idOrEmail: string) {
+		return this.userService.findOne(idOrEmail);
+	}
+
+	@Delete(':id')
+	deleteUser(@Param('id', ParseUUIDPipe) id: string) {
+		return this.userService.delete(id);
+	}
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { PrismaService } from '@prisma/prisma.service';
+import { pbkdf2Sync } from 'node:crypto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+	constructor(private readonly prismaService: PrismaService) {}
+
+	public save = (user: Partial<User>) => {
+		const hashedPassword = this.hashPassword(user.password);
+		return this.prismaService.user.create({
+			data: {
+				email: user.email,
+				password: hashedPassword,
+				roles: ['USER'],
+			},
+		});
+	};
+	public findOne = (idOrEmail: string) => {
+		return this.prismaService.user.findFirst({
+			where: {
+				OR: [{ id: idOrEmail }, { email: idOrEmail }],
+			},
+		});
+	};
+	public delete = (id: string) => {
+		return this.prismaService.user.delete({
+			where: { id },
+		});
+	};
+	private hashPassword = (password: string): string => {
+		return pbkdf2Sync(
+			password,
+			'saltExample',
+			10000,
+			64,
+			'sha256',
+		).toString('hex');
+	};
+}
